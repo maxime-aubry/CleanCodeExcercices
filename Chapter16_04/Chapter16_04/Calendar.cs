5d18c851bbee375a0bc8c36f13ccceec61921727
Maxime Aubry <maxime.aubry.76@gmail.com>
2022-03-10 22:26:10 +0100

    Chapter16_04
    création des factories

---
+++ Chapter16_04/Chapter16_04/Calendar.cs
@@ -0,0 +1,13 @@
+﻿namespace Chapter16_04
+{
+    public class Calendar
+    {
+        public const int SUNDAY = 1;
+        public const int MONDAY = 2;
+        public const int TUESDAY = 3;
+        public const int WEDNESDAY = 4;
+        public const int THURSDAY = 5;
+        public const int FRIDAY = 6;
+        public const int SATURDAY = 7;
+    }
+}
---
+++ Chapter16_04/Chapter16_04/Factories/DayDateFactory.cs
@@ -0,0 +1,29 @@
+﻿using Chapter16_04.Enums;
+using System;
+
+namespace Chapter16_04.Factories
+{
+    public abstract class DayDateFactory
+    {
+        private static DayDateFactory factory = new SpreadsheetDateFactory();
+
+        public static void setInstance(DayDateFactory factory)
+        {
+            DayDateFactory.factory = factory;
+        }
+
+        protected abstract DayDate _makeDate(int ordinal);
+        protected abstract DayDate _makeDate(int day, Month month, int year);
+        protected abstract DayDate _makeDate(int day, int month, int year);
+        protected abstract DayDate _makeDate(DateTime date);
+        protected abstract int _getMinimumYear();
+        protected abstract int _getMaximumYear();
+
+        public static DayDate makeDate(int ordinal) => factory._makeDate(ordinal);
+        public static DayDate makeDate(int day, Month month, int year) => factory._makeDate(day, month, year);
+        public static DayDate makeDate(int day, int month, int year) => factory._makeDate(day, month, year);
+        public static DayDate makeDate(DateTime date) => factory._makeDate(date);
+        public static int GetMinimumYear() => factory._getMinimumYear();
+        public static int GetMaximumYear() => factory._getMaximumYear();
+    }
+}
